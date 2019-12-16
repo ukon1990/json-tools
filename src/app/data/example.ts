39ba4fde672d958638cbdf93b7f49f4942181316
Ukon1990 <mail@jonaskf.net>
2019-12-16 18:20:00 +0100

    Updated example objects and about

--- src/app/data/example.ts
+++ src/app/data/example.ts
@@ -3,11 +3,12 @@ export const exampleData = {
     name: 'Åge',
     age: 99,
     profession: 'Snake tamer',
-    childFrom: {val: 9}
+    childFrom: {val: 9},
+    childTo: {something: false}
   },
   to: {
     profession: 'Carpenter',
     favoriteDrink: 'Water',
-    childTo: {value: 1}
+    childTo: {value: 1, something: true}
   }
 };
