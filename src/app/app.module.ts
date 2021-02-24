a4fe44f24aaa9e6b7fd09eb5505080c38c6c8c97
Ukon <mail@jonaskf.net>
2021-02-24 08:41:17 +0100

    import fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,25 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatCardModule} from '@angular/material/card';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatSelectModule} from '@angular/material/select';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {ServiceWorkerModule} from '@angular/service-worker';
+import {Angulartics2Module} from 'angulartics2';
+import {NgxJsonViewerModule} from 'ngx-json-viewer';
+import {HttpClientModule} from '@angular/common/http';
+import {ReactiveFormsModule} from '@angular/forms';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './components/app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {ServiceWorkerModule} from '@angular/service-worker';
 import {environment} from '../environments/environment';
-import {Angulartics2Module} from 'angulartics2';
-import {NgxJsonViewerModule} from 'ngx-json-viewer';
 import { ViewerComponent } from './components/viewer/viewer.component';
-import {HttpClientModule} from '@angular/common/http';
-import {ReactiveFormsModule} from '@angular/forms';
-import {
-  MatButtonModule, MatCardModule,
-  MatExpansionModule,
-  MatFormFieldModule,
-  MatInputModule, MatMenuModule,
-  MatSelectModule,
-  MatTabsModule,
-  MatToolbarModule
-} from '@angular/material';
 import { AboutComponent } from './components/about/about.component';
 import {HeaderComponent} from './components/header/header.component';
 import {MenuComponent} from './components/header/menu/menu.component';
